4c877fa9d20f96818c413f5c353b391245f41e00
Gabriel Santiago <vcgabriel@outlook.com>
2025-03-24 00:16:45 -0300

    fix fopen

--- app/projeto.c
+++ app/projeto.c
@@ -8,8 +8,7 @@
 int main()
 {
     Materia disciplinas[46];
-    FILE *arquivo = fopen("materias.txt", "r");
-    int periodo[11][6];
+    FILE *arquivo = fopen("../input/materias.txt", "r");    int periodo[11][6];
     for (int i = 0; i < 11; i++)
         for (int j = 0; j < 6; j++)
             periodo[i][j] = -1;
